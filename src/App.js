94650aa4a4b226023677ffb076a7f9f09bf38806
kass8mal8 <kassimaly21@gmail.com>
2022-07-09 10:01:12 +0300

    2 files modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import SearchData from "./SearchData";
 import "./style.css";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <SearchData/>
     </div>
   );
 }
--- src/SearchData.js
+++ src/SearchData.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import UseFetch from './UseFetch'
+import React, {useState, useEffect} from 'react'
 
 const SearchData =()=>{
-  const url="https://api.github.com/users/"
-  const [data]=UseFetch(url)
+  const url="https://shoppingapiacme.herokuapp.com/shopping "
+  const [data, setData]=useState([])
 
+  useEffect(()=>{
+    const getData=async()=>{
+      try{
+        const res=await fetch(url)
+        const data=await res.json()
+
+        setData(data)
+        console.log(data)
+      }
+      catch(e){
+        console.log(e.message)
+      }
+    }
+    getData()
+  },[url])
   return(
     <div>
-      
+      <p>Fetching data from api</p>
+      {data.map(product=>(
+        <div>
+         <p> {product.brand}</p>
+        </div>
+      ))}
     </div>
   )
 }
+
+export default SearchData
