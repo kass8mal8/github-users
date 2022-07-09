c9e875ea64b1dc8b18257dd384a085256f507d3d
kass8mal8 <kassimaly21@gmail.com>
2022-07-09 15:09:12 +0300

    2 files modified

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import React,{useState, useEffect } from 'react'
 
 const Home=()=>{
   const [username, setName] =useState('')
+  const [data, setData]=useState([])
 
   const getUser =async(username)=>{
     const url=`https://api.github.com/users/${username}`
@@ -10,20 +11,23 @@ const Home=()=>{
           const res=await fetch(url)
           const data=await res.json()
           console.log(data)
-
+          setData(data)
         }
         catch(error){
           console.log(error.message)
         }
   } 
     useEffect(()=>{
-           getUser (username)
-
+        getUser (username)
      },[username])
   
   return(
     <div className="container">
       <input type="text" placeholder="enter username to search" onChange={(e)=>setName(e.target.value)} />
+
+          <p>{data.name}</p>
+          <img src={data.avatar_url} />
+      
     </div>
   )
 }
