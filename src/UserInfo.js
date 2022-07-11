d45d3e6912c24eaab8555ec11f467d446b2e0f87
kassimaly21@gmail.com <kassimaly21@gmail.com>
2022-07-11 10:44:10 +0300

    2 files modified

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,5 +1,11 @@
 import React from 'react'
+
 const UserInfo = ({ data }) => {
+   const intro_styles=()=>{
+      color:'royalblue';
+      marginTop:'0px';
+      fontSize:'13px'
+   }
    return (
    <div>
      {data &&
@@ -7,18 +13,25 @@ const UserInfo = ({ data }) => {
         <section>
            <img
              src={data.avatar_url}
-             width="100"/>
+             width="90"/>
          </section>
          <section className="intro" >
-           <p> {data.name} <br/>
-             {data.location} </p>
-           
-            {data.bio == null ? 
-               <p> This profile has no bio</p> 
-            :  <p>  {data.bio}  </p> } 
-           {data.created_at && 
-             <p> Joined on {data.created_at} </p>
-           } 
+           <aside>
+              <h3> {data.name} </h3>
+              <p style={intro_styles}>
+                @{data.login} 
+              </p>
+              
+               {data.bio ? 
+                <p>  {data.bio}  </p> :
+                <p> This profile has no bio</p> 
+               } 
+            </aside>
+            <aside>
+              {data.created_at && 
+                <p> Joined  {data.created_at} </p>
+              } 
+           </aside>
          </section>
      </div>
      } 
