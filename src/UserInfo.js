e2deb27791dd62c1edc90c6ff0d8d78deff3f908
kassimaly21@gmail.com <kassimaly21@gmail.com>
2022-07-11 09:29:40 +0300

    1 file modified

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,26 +1,28 @@
 import React from 'react'
 const UserInfo = ({ data }) => {
    return (
-      <div>
-     {data ?
+   <div>
+     {data &&
      <div className="user-container"> 
-     <section>
-        <img
-          src={data.avatar_url}
-          width="100"/>
-      </section>
-      <section className="intro" >
-        <p>{data.name}
-        <br/>
-         {data.location} 
-         {data.bio} 
-        </p>
-        <p>
-         {data.created_at}
-        </p>
-      </section>
-      </div> : ""} 
-    </div>
+        <section>
+           <img
+             src={data.avatar_url}
+             width="100"/>
+         </section>
+         <section className="intro" >
+           <h3> {data.name} </h3>
+           <p>  {data.location} </p>
+           
+            {data.bio == null ? 
+               <p> This profile has no bio</p> 
+            :  <p>  {data.bio}  </p> } 
+           {data.created_at && 
+             <p> Joined on {data.created_at} </p>
+           } 
+         </section>
+     </div>
+     } 
+   </div>
    )
 }
 
