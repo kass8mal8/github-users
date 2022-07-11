29c45cce87ccfac9d3988c8264d44bbfec69a5cc
kassimaly21@gmail.com <kassimaly21@gmail.com>
2022-07-11 09:05:28 +0300

    modified various files

--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,55 @@
-import React,{useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import UserInfo from './UserInfo'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons' 
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 
-const Home=()=>{
-  const [username, setName] =useState('')
-  const [data, setData]=useState([])
+const Home = () => {
+   const [username, setName] = useState('')
+   const [data, setData] = useState([])
 
-  const getUser =async(username)=>{
-    const url=`https://api.github.com/users/${username}`
-        try{         
-          const res=await fetch(url)
-          const data=await res.json()
-          console.log(data)
-          setData(data)
-        }
-        catch(error){
-          console.log(error.message)
-        }
-  } 
-    useEffect(()=>{
-        getUser (username)
-     },[username])
-  
-  const nameRef=useRef()
-  const handleSearch =() =>{
-    setName(nameRef.current.value)
-  } 
-  return(
-    <div className="container">
+   const getUser = async (username) => {
+      const url = `https://api.github.com/users/${username}`
+      try {
+         const res = await fetch(url)
+         const data = await res.json()
+         console.log(data)
+         setData(data)
+      }
+      catch (error) {
+         console.log(error.message)
+      }
+   }
+   useEffect(() => {
+      getUser(username)
+   }, [username])
+
+   const nameRef = useRef()
+   const handleSearch = () => {
+      setName(nameRef.current.value)
+   }
+   return (
+      <div className="container">
       <header>
-        <input type="text" placeholder="search github username... " ref={nameRef} />
+          <input 
+            type="text"
+            placeholder="search github username... "
+            ref={nameRef} />
     
           <FontAwesomeIcon 
              icon={faSearch}    
              className="search-btn"/>
-             <button onClick ={handleSearch}>search</button>
+             
+          <button 
+             onClick ={handleSearch}>
+             search
+          </button>
     </header>
     
      <UserInfo data={data} />
     </div>
-  )
+   )
 }
 
 export default Home
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,21 +1,27 @@
 import React from 'react'
-const UserInfo=({data} )=>{
-  return(
-    <div>
+const UserInfo = ({ data }) => {
+   return (
+      <div>
      {data ?
-     <div className="user-container"> <section>
+     <div className="user-container"> 
+     <section>
         <img
           src={data.avatar_url}
           width="100"/>
       </section>
-      <section>
-       <p>{data.name}<br/>
-       {data.location} 
-      {data.bio} </p>
-      <p>{data.created_}</p>
-      </section></div> : ""} 
+      <section className="intro" >
+        <p>{data.name}
+        <br/>
+         {data.location} 
+         {data.bio} 
+        </p>
+        <p>
+         {data.created_at}
+        </p>
+      </section>
+      </div> : ""} 
     </div>
-  )
+   )
 }
 
 export default UserInfo
